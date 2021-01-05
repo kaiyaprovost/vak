c113dc4cae71ac2aceb0cc24818d526dd77a82a1
David Nicholson <nickledave@users.noreply.github.com>
2021-01-04 19:43:48 -0500

    add is_a_directory function to vak.validation

--- src/vak/validation.py
+++ src/vak/validation.py
@@ -3,6 +3,7 @@
 adapted in part from scikit-learn under license
 https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/utils/validation.py
 """
+from pathlib import Path, PurePath
 import warnings
 
 import numpy as np
@@ -60,3 +61,36 @@ def row_or_1d(y, warn=False):
         return np.ravel(y)
 
     raise ValueError("bad input shape {0}".format(shape))
+
+
+def is_a_directory(dir_path):
+    """validate that ``dir_path`` is a directory.
+
+    Handles strings that end in `/**`,
+    which indicates "recursively search this directory",
+    by removing that "suffix" and checking that
+    what precedes it is an existing directory.
+
+    Parameters
+    ----------
+    dir_path: str, pathlib.Path
+    """
+    if not (isinstance(dir_path, str) or isinstance(dir_path, PurePath)):
+        raise TypeError(
+            f'path must be a string or pathlib.Path, but type was {type(dir_path)}'
+        )
+
+    if isinstance(dir_path, PurePath):
+        dir_path = str(dir_path)
+
+    if dir_path.endswith('/**'):
+        dir_to_validate = dir_path[:-2]  # remove terminal asterisks
+    else:
+        dir_to_validate = dir_path
+
+    if not Path(dir_to_validate).exists():
+        raise NotADirectoryError(
+            f'path was not recognized as a directory: {dir_path}'
+        )
+    else:
+        return True
